5965cea37ca5e783be4b4e02751830895b728a21
anongnad <taranongnad@gmail.com>
2018-11-27 15:20:24 +0700

    11/27/2018

---
+++ lab1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        lbl5.Text = Val(tb1.Text) * 12
+        lbl6.Text = Val(tb1.Text) * 12 * 0.05
+        lbl7.Text = (Val(tb1.Text) * 12) - (Val(tb1.Text) * 12 * 0.05)
+
+    End Sub
+End Class
